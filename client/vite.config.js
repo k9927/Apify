5153b147f3e19872c860a903a2d73687886edc53
Karandeep <bhasinkarandeepsingh826@gmail.com>
2025-08-06 21:20:08 +0530

    Fixing Errors

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/client/', // Critical for Vercel
   plugins: [react()],
+  build: {
+    outDir: '../dist', // Build outside client folder
+    emptyOutDir: true
+  },
   server: {
     proxy: {
       '/api': {
         target: 'https://api.apify.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '/v2'),
-      },
-    },
-  },
+      }
+    }
+  }
 })
